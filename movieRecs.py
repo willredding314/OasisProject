f70643467f42e69f784956daa2b16342b41e3b8d
Will Redding <willredding3.14@gmail.com>
2022-02-04 23:12:12 -0500

    Add index file for hosting

--- movieRecs.py
+++ movieRecs.py
@@ -1,4 +1,24 @@
 
+import json
+import requests 
+#need to install -- pip3 install requests 
+#use as requests.get("url")
+
+################################
+#             TMDB             #
+#                              #
+# user: oasisproj13            #
+# pw: oasis                    #
+# setup through will's email   #
+#                              #
+################################
 
 
 api_key = "k_rxh9t80o"
+
+f = open("sampleData.json")
+sampleData = json.load(f)
+top250 = sampleData["items"]
+
+
+
