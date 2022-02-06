f4866fbe99c5b83287820a0391d7a46b971462ca
Will Redding <willredding3.14@gmail.com>
2022-02-06 13:44:37 -0500

    add notes for installs

--- userData.py
+++ userData.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+####  INSTALLS NEEDED
+# git - can then clone the repo
+# pip - for all python downloads $ pip install NAME
+#      - python
+#      - numpy
+#      - requests
+
+
 
 startData = [0.5] * 10
 userVector = np.array(startData)
