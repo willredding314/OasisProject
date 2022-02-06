caa2ad0ce67ee60db68de9413f7c4ef31f3cbf09
Will Redding <willredding3.14@gmail.com>
2022-02-06 13:25:44 -0500

    Setup user data file

--- userData.py
+++ userData.py
@@ -1,1 +1,12 @@
+import numpy as np
+
+
+startData = [0.5] * 10
+userVector = np.array(startData)
+
 #File to get user data in vector form
+print("Do you like horror?")
+response = input()
+userVector[0] = response
+print(userVector[0])
+
