87fb97d73fb957141891050f7cbdec4f6997519a
Will Redding <willredding@Wills-MacBook-Pro.local>
2022-03-06 12:26:20 -0500

    Update django settings for admin

--- django-module/mysite/recsapp/models.py
+++ django-module/mysite/recsapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class MovieData(models.Model):
+    hard_filters = models.JSONField()
+    vector_data = models.JSONField()
+    cast_data = models.JSONField()
+
+
+class BinarySelection(models.Model):
+    question_text = models.CharField(max_length=200)
+    response = models.BooleanField()
+
+    def __str__(self):
+        return self.question_text + " - " + str(self.response)
+
+class TextSelection(models.Model):
+    question = models.CharField(max_length=200)
+    response = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question + " - " + str(self.response)
+
+
